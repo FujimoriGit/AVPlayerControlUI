26ea6523aee489654280b1239c1294523cb4ddaf
Daiki Fujimori <G016C1102@outlook.jp>
2026-05-09 12:05:08 +0900

    Initial Commit

---
+++ AVPlayerControlUITests/AVPlayerControlUITests.swift
@@ -0,0 +1,18 @@
+//
+//  AVPlayerControlUITests.swift
+//  AVPlayerControlUITests
+//  
+//  Created by Daiki Fujimori on 2026/05/09
+//  
+
+
+import Testing
+@testable import AVPlayerControlUI
+
+struct AVPlayerControlUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
